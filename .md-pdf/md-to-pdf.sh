c1ef61733b21953373064918090ca06b17fc6e03
小尾 渉太 <shota.obi@neosystem.co.jp>
2024-12-16 11:59:23 +0900

    z

--- .md-pdf/md-to-pdf.sh
+++ .md-pdf/md-to-pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 shopt -s globstar
-          npx md-to-pdf-ng --config-file ./.md-pdf/config.json ${{ env.NESTED_OUTPUT_DIRECTORY }}/*.md
+          npx md-to-pdf-ng --config-file ./.md-pdf/config.json ./docs/*.md
